55f5dc221b1fb09abedfae0318a894c139ddc936
warrockhali <warrockhali@gmail.com>
2026-01-14 14:10:25 +0900

    [BE] feat: Spring Event 기반 알림 기능 구현 (#56)

---
+++ backend/src/main/java/back/listener/NotificationEventListener.java
@@ -0,0 +1,51 @@
+package back.listener;
+
+import back.domain.Notifications;
+import back.dto.NotificationResponse;
+import back.event.ScheduleRegisteredEvent;
+import back.repository.clubs.ClubMembersRepository;
+import back.repository.notifications.NotificationsRepository;
+import back.service.notifications.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class NotificationEventListener {
+
+    private final ClubMembersRepository clubMembersRepository;
+    private final NotificationsRepository notificationsRepository;
+    private final NotificationService notificationService;
+
+    @Async
+    @TransactionalEventListener
+    public void handleScheduleRegisteredEvent(ScheduleRegisteredEvent scheduleRegisteredEvent) {
+        //TODO : 활성화 상태인 멤버 리스트를 조회하는 로직으로 교체 필요
+        //더미 데이터
+        List<Long> memberIds = List.of(1L, 2L, 3L);
+
+        List<Notifications> list = new ArrayList<>();
+        for (int i = 0; i < memberIds.size(); i++) {
+            list.add(new Notifications(
+                    memberIds.get(i),
+                    "새로운 일정 '" + scheduleRegisteredEvent.getScheduleName() + "'이 등록 되었습니다.",
+                    scheduleRegisteredEvent.getScheduleId(),
+                    "SCHEDULE"
+                    ));
+        }
+        List<Notifications> notifications = notificationsRepository.saveAll(list);
+
+        for (Notifications notification : notifications) {
+            NotificationResponse response = NotificationResponse.from(notification);
+            notificationService.send(notification.getUserId(), response);
+        }
+
+
+    }
+}
